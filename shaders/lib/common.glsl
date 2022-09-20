44559268da87575ae8a203e149e7e75000d0df7e
Septonious <83358692+Septonious@users.noreply.github.com>
2022-09-20 19:47:31 +0300

    Update common.glsl

--- shaders/lib/common.glsl
+++ shaders/lib/common.glsl
@@ -73,7 +73,7 @@ const float shadowBlurStrength = 1.0 / shadowMapResolution;
 //Volumetric Light
 #define VL
 #define VL_OPACITY 0.50 //[0.10 0.15 0.20 0.25 0.30 0.35 0.40 0.45 0.50 0.55 0.60 0.65 0.70 0.75 0.80 0.85 0.90 0.95 1.00]
-#define VL_SAMPLES 20 //[2 3 4 5 6 7 8 9 10 11 12 13 14 15 16]
+#define VL_SAMPLES 10 //[2 3 4 5 6 7 8 9 10 11 12 13 14 15 16]
 
 //Volumetric Clouds
 #define VC
