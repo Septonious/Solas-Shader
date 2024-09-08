23e413b8ec326017ddb6076194b08ffbed7ddb76
Septonious <83358692+Septonious@users.noreply.github.com>
2024-09-08 12:13:26 +0300

    more ipbr blocks

--- shaders/lib/pbr/blocks/concrete.glsl
+++ shaders/lib/pbr/blocks/concrete.glsl
@@ -1,5 +1,9 @@
-#ifdef EMISSIVE_CONCRETE
 if (material >= 15 && material <= 20) {
+    #ifdef EMISSIVE_CONCRETE    
     emission = lAlbedo;
+    #endif
+
+    smoothness = clamp(lAlbedo * 0.1 + 0.1, 0.06, 0.3);
+} else if (material2 == 318) {
+    smoothness = clamp(lAlbedo * 0.1 + 0.1, 0.06, 0.3);
 }
-#endif
