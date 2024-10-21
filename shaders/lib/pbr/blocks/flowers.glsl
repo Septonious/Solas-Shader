0b0d7545b164e1024a3e1665cb4eaa5aedc74dc6
Septonious <83358692+Septonious@users.noreply.github.com>
2024-10-21 19:20:37 +0300

    Update flowers.glsl

--- shaders/lib/pbr/blocks/flowers.glsl
+++ shaders/lib/pbr/blocks/flowers.glsl
@@ -1,12 +1,15 @@
 if (material >= 35 && material <= 40) {
-	if (albedo.b > albedo.g || albedo.r > albedo.g) {
-		emission = lAlbedo;
-		emission = max(emission, 0.0);
+	if (albedo.r + albedo.g + albedo.b < 2.2) {
+		if (albedo.b > albedo.g || albedo.r > albedo.g) {
+			emission = lAlbedo;
+			emission = max(emission, 0.0);
+		}
+		if (material == 39 && albedo.g - albedo.r < 0.2) {
+			emission = int(lAlbedo > 0.9);
+		}
 	}
-    if (material == 39 && albedo.g - albedo.r < 0.2) {
-        emission = int(lAlbedo > 0.9);
-    }
+
 	#ifdef OVERWORLD
-	emission *= mix(0.125, 0.0, wetness);
+	emission *= mix(0.1, 0.0, wetness);
 	#endif	
 }
