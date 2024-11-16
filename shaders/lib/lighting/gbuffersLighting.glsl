70a23a1512915dc8da5bb8b73c75c19aafb06724
Septonious <83358692+Septonious@users.noreply.github.com>
2024-11-16 16:29:33 +0300

    Update gbuffersLighting.glsl

--- shaders/lib/lighting/gbuffersLighting.glsl
+++ shaders/lib/lighting/gbuffersLighting.glsl
@@ -157,7 +157,7 @@ void gbuffersLighting(inout vec4 albedo, in vec3 screenPos, in vec3 viewPos, in 
     #if (defined GBUFFERS_TERRAIN || defined GBUFFERS_ENTITIES || defined GBUFFERS_BLOCK) && !defined NETHER
 	vec3 baseReflectance = vec3(0.1);
 
-    float smoothnessF = 0.1 + length(albedo.rgb) * 0.3 + originalNoL * 0.2 + float(subsurface > 0.0) * 0.15;
+    float smoothnessF = 0.1 + length(albedo.rgb) * 0.3 + originalNoL * 0.2;
           smoothnessF = mix(smoothnessF, 0.95, smoothness);
 
     #ifdef OVERWORLD
