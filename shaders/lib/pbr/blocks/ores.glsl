b1f8ec673ce3431ceeb39c9265fcdffcf4365073
ardemanse <septonious@proton.me>
2024-12-08 16:28:18 +0300

    fixed optifine compatibility

--- shaders/lib/pbr/blocks/ores.glsl
+++ shaders/lib/pbr/blocks/ores.glsl
@@ -8,7 +8,7 @@ if (material == 23) {
 	emission -= int((albedo.r < 0.4 && albedo.g < 0.55 && albedo.b < 0.56) && (albedo.r > 0.38 && albedo.g > 0.53 && albedo.b > 0.54));
 	emission = clamp(emission * int(emission > 0.1) * 16.0, 0.0, 1.0) * lAlbedo * 0.25;
 }
-# Zinc
+
 if (material == 72) {
 	emission = clamp(max(max(max(albedo.r - albedo.b, albedo.r - albedo.g), max(albedo.b - albedo.g, albedo.b - albedo.r)), max(albedo.g - albedo.r, albedo.g - albedo.b)), 0.0, 1.0);
 	emission = clamp(emission * int(emission > 0.021) * 16.0, 0.0, 1.0) * lAlbedo * 0.25;
