b71aa8ba49bbe4f3847f9d6251f6378225c65d7f
Lompa .J <lompaj@protonmail.com>
2025-03-11 11:46:09 -0300

    replace min and max functions with clamp for clarity and consiceness

--- shaders/lib/lighting/handlight.glsl
+++ shaders/lib/lighting/handlight.glsl
@@ -2,8 +2,8 @@ void getHandLightColor(inout vec3 blockLighting, in vec3 normal, in vec3 pos) {
     if ((heldItemId >= 1 && heldItemId <= 60) || (heldItemId2 >= 1 && heldItemId2 <= 60)) {
         float handlight = clamp((32.0 - length(pos) * 5.0) * 0.015, 0.0, 1.0);
 
-        vec3 color1 = blocklightColorArray[min(max(heldItemId - 1, 0), blocklightColorArray.length() - 1)];
-        vec3 color2 = blocklightColorArray[min(max(heldItemId2 - 1, 0), blocklightColorArray.length() - 1)];
+        vec3 color1 = blocklightColorArray[clamp(heldItemId - 1, 0, blocklightColorArray.length() - 1)];
+        vec3 color2 = blocklightColorArray[clamp(heldItemId2 - 1, 0, blocklightColorArray.length() - 1)];
         vec3 handLightColor = mix(color1, color2, 0.5);
              handLightColor *= length(color1) + length(color2);
 
