ee8f6212182c36d9f61a77d396fc676ef7aa7bea
Septonious <83358692+Septonious@users.noreply.github.com>
2025-03-18 23:56:59 +0300

    Update flowers.glsl

--- shaders/lib/pbr/blocks/flowers.glsl
+++ shaders/lib/pbr/blocks/flowers.glsl
@@ -1,7 +1,8 @@
 else if (material >= 35 && material < 40) {
+	float emissionFactor = lAlbedo * (0.1 + (1.0 - clamp(length(viewPos) * 0.4, 0.0, 1.0)));
 	if (albedo.r + albedo.g + albedo.b < 2.9) {
 		if (albedo.b > albedo.g || albedo.r > albedo.g) {
-			emission = lAlbedo * 0.15;
+			emission = emissionFactor;
 			if (material == 36) emission *= 0.5;
 			#ifdef OVERWORLD
 			emission *= mix(1.0, 0.0, wetness);
@@ -9,24 +10,26 @@ else if (material >= 35 && material < 40) {
 		}
 	}
 	if (material == 39 && lAlbedo > 0.99) {
-		emission = lAlbedo * lAlbedo * 0.15;
+		emission = emissionFactor * lAlbedo;
 		#ifdef OVERWORLD
 		emission *= mix(1.0, 0.0, wetness);
 		#endif
 	}
 } else if (material == 40) {
+	float emissionFactor = lAlbedo * (0.1 + (1.0 - clamp(length(viewPos) * 0.4, 0.0, 1.0)));
 	if (albedo.r + albedo.g + albedo.b < 2.9) {
 		if (albedo.b > albedo.g || albedo.r > albedo.g) {
-			emission = lAlbedo * 0.15 * float(albedo.r > 0.6 && albedo.b < 0.3);
+			emission = emissionFactor * float(albedo.r > 0.6 && albedo.b < 0.3);
 			#ifdef OVERWORLD
 			emission *= mix(1.0, 0.0, wetness);
 			#endif
 		}
 	}
 } else if (material >= 74 && material <= 79) {
+	float emissionFactor = lAlbedo * (0.1 + (1.0 - clamp(length(viewPos) * 0.4, 0.0, 1.0)));
 	if (albedo.r + albedo.g + albedo.b < 2.9) {
 		if (albedo.b > albedo.g || albedo.r > albedo.g) {
-			emission = lAlbedo * 0.15 * (float(lAlbedo > 0.63) * (float(albedo.r + albedo.g * 0.5 + albedo.b * 0.5 > 0.9) + float(albedo.r - albedo.g - albedo.b > 0.3)));
+			emission = emissionFactor * (float(lAlbedo > 0.63) * (float(albedo.r + albedo.g * 0.5 + albedo.b * 0.5 > 0.9) + float(albedo.r - albedo.g - albedo.b > 0.3)));
 			#ifdef OVERWORLD
 			emission *= mix(1.0, 0.0, wetness);
 			#endif
