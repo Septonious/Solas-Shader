c3b597dbdd565be7f43a6ff30bc2dd9822d13a51
Septonious <83358692+Septonious@users.noreply.github.com>
2025-03-21 19:58:34 +0300

    Update flowers.glsl

--- shaders/lib/pbr/blocks/flowers.glsl
+++ shaders/lib/pbr/blocks/flowers.glsl
@@ -1,8 +1,10 @@
 else if (material >= 35 && material < 40) {
 	float emissionFactor = lAlbedo * (0.1 + (1.0 - clamp(length(viewPos) * 0.4, 0.0, 1.0)));
+	float noise = texture2D(noisetex, (worldPos.xz + cameraPosition.xz + frameCounter * 0.01) * 0.001).b;
+		  noise = 0.75 + clamp(noise - 0.4, 0.0, 1.0);
 	if (albedo.r + albedo.g + albedo.b < 2.9) {
 		if (albedo.b > albedo.g || albedo.r > albedo.g) {
-			emission = emissionFactor;
+			emission = emissionFactor * noise;
 			if (material == 36) emission *= 0.5;
 			#ifdef OVERWORLD
 			emission *= mix(1.0, 0.0, wetness);
@@ -17,9 +19,11 @@ else if (material >= 35 && material < 40) {
 	}
 } else if (material == 40) {
 	float emissionFactor = lAlbedo * (0.1 + (1.0 - clamp(length(viewPos) * 0.4, 0.0, 1.0)));
+	float noise = texture2D(noisetex, (worldPos.xz + cameraPosition.xz + frameCounter * 0.01) * 0.001).b;
+		  noise = 0.75 + clamp(noise - 0.4, 0.0, 1.0);
 	if (albedo.r + albedo.g + albedo.b < 2.9) {
 		if (albedo.b > albedo.g || albedo.r > albedo.g) {
-			emission = emissionFactor * float(albedo.r > 0.6 && albedo.b < 0.3);
+			emission = emissionFactor * noise * float(albedo.r > 0.6 && albedo.b < 0.3);
 			#ifdef OVERWORLD
 			emission *= mix(1.0, 0.0, wetness);
 			#endif
@@ -27,9 +31,11 @@ else if (material >= 35 && material < 40) {
 	}
 } else if (material >= 74 && material <= 79) {
 	float emissionFactor = lAlbedo * (0.1 + (1.0 - clamp(length(viewPos) * 0.4, 0.0, 1.0)));
+	float noise = texture2D(noisetex, (worldPos.xz + cameraPosition.xz + frameCounter * 0.01) * 0.001).b;
+		  noise = 0.75 + clamp(noise - 0.4, 0.0, 1.0);
 	if (albedo.r + albedo.g + albedo.b < 2.9) {
 		if (albedo.b > albedo.g || albedo.r > albedo.g) {
-			emission = emissionFactor * (float(lAlbedo > 0.63) * (float(albedo.r + albedo.g * 0.5 + albedo.b * 0.5 > 0.9) + float(albedo.r - albedo.g - albedo.b > 0.3)));
+			emission = emissionFactor * noise * (float(lAlbedo > 0.63) * (float(albedo.r + albedo.g * 0.5 + albedo.b * 0.5 > 0.9) + float(albedo.r - albedo.g - albedo.b > 0.3)));
 			#ifdef OVERWORLD
 			emission *= mix(1.0, 0.0, wetness);
 			#endif
