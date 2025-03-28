ae76eb412db1f9c719e5550b3c704a4a72cde337
Septonious <83358692+Septonious@users.noreply.github.com>
2025-03-29 01:21:04 +0300

    Update concrete.glsl

--- shaders/lib/pbr/blocks/concrete.glsl
+++ shaders/lib/pbr/blocks/concrete.glsl
@@ -5,5 +5,5 @@ else if (material >= 15 && material <= 20) {
 
     smoothness = clamp(lAlbedo * 0.4, 0.0, 0.5);
 } else if (material2 == 318) {
-    smoothness = clamp(lAlbedo * 0.2, 0.0, 0.5);
+    smoothness = clamp(lAlbedo * 0.4, 0.0, 0.5);
 }
