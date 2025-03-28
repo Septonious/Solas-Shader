aae1dac0810940c38213e8969d4cdca2ed139a91
Septonious <83358692+Septonious@users.noreply.github.com>
2025-03-29 01:16:29 +0300

    ender black hole rewrite

--- shaders/lib/atmosphere/endVortex.glsl
+++ shaders/lib/atmosphere/endVortex.glsl
@@ -6,31 +6,30 @@ vec3 getSpiral(vec2 coord, float hole) {
     float center = pow8(1.0 - coord.y) * 24.0;
     float spiral = sin((coord.x + sqrt(coord.y) * whirl) * arms) + center - coord.y;
 
-    return clamp(endAmbientColSqrt * spiral * 0.15, 0.0, 1.0);
+    return clamp(endAmbientColSqrt * spiral * 0.125, 0.0, 1.0);
 }
 
 void getEndVortex(inout vec3 color, in vec3 worldPos, in vec3 stars, in float VoU, in float VoS) {
 	if (0.5 < VoS) {
 		vec3 sunVec = mat3(gbufferModelViewInverse) * sunVec;
 		vec2 sunCoord = sunVec.xz / (sunVec.y + length(sunVec));
-		vec2 planeCoord0 = worldPos.xz / (worldPos.y + length(worldPos)) + sunCoord;
-			 planeCoord0.x += 0.5;
-			 planeCoord0.y -= 0.23;
 		vec2 planeCoord1 = worldPos.xz / (worldPos.y + length(worldPos)) - sunCoord;
-		vec2 center = vec2(0.5);
-		
-		float dist = distance(planeCoord0, center);
-		float invDist = 1.0 - dist;
-		float ring = pow(smoothstep(0.3, 0.05, dist * 1.5) * 4.0, 3.5) + 1.0;
 
-		float hole = step(0.05, dist);
-			  hole *= smoothstep(0.085, 0.100, dist);
+		float ring1 = pow24(pow32(VoS)) * 10000.0;
+		float ring2 = pow20(pow32(VoS)) * 10000.0 - ring1 * 5.0;
+		float ring3 = pow32(pow32(VoS)) * 2000.0;
+		float ring4 = pow24(pow32(VoS)) * 900.0 - ring3 * 5.4;
+		float rings = clamp(ring2, 0.0, 1.0) + clamp(pow5(ring4) * 400000, 0.0, 1.0);
 
-		vec3 accretionDisk = endLightCol * pow7(invDist) * 0.25;
+		float hole = pow32(pow32(VoS)) * 5000000.0;
+			  hole = clamp(hole, 0.0, 1.0);
+
+		vec3 accretionDisk = endLightCol * 12.0;
 		vec3 spiral = getSpiral(planeCoord1, VoS);
 
 		color = mix(color, spiral, length(spiral));
-		color += clamp(ring * hole * accretionDisk, 0.0, 1.0);
-		color *= mix(1.0, 0.0, float(0.97 < VoS) * (1.0 - hole));
+		color += endLightCol * pow8(VoS) * 0.075;
+		color *= 1.0 - hole;
+		color += clamp(rings * accretionDisk, 0.0, 1.0);
 	}
 }
--- shaders/lib/common.glsl
+++ shaders/lib/common.glsl
@@ -4,11 +4,7 @@
 
 const int shadowMapResolution = 2048; //[512 1024 2048 4096]
 const float shadowDistance = 192.0; //[128.0 192.0 256.0 320.0 384.0 448.0 512.0 1024.0]
-#ifndef END
 const float sunPathRotation = -40.0; //[-85.0 -80.0 -75.0 -70.0 -65.0 -60.0 -55.0 -50.0 -45.0 -40.0 -35.0 -30.0 -25.0 -20.0 -15.0 -10.0 -5.0 0.0 5.0 10.0 15.0 20.0 25.0 30.0 35.0 40.0 45.0 50.0 55.0 60.0 65.0 70.0 75.0 80.0 85.0]
-#else
-const float sunPathRotation = -40.0;
-#endif
 const float shadowMapBias = 1.0 - 25.6 / shadowDistance;
 
 #define REALTIME_SHADOWS
