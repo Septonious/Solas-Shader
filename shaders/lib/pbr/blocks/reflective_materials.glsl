816421e4c9d293fb593a87cfe43097cca9454c49
Septonious <83358692+Septonious@users.noreply.github.com>
2025-03-29 14:48:54 +0300

    Update reflective_materials.glsl

--- shaders/lib/pbr/blocks/reflective_materials.glsl
+++ shaders/lib/pbr/blocks/reflective_materials.glsl
@@ -1,5 +1,5 @@
 else if (material2 == 301) {// Iron, Gold, Emerald, Diamond, Copper & Plates
-    smoothness = pow2(lAlbedo) * 0.7;
+    smoothness = 0.3 + pow2(lAlbedo) * 0.5;
 } else if (material2 == 299) {
     smoothness = pow3(lAlbedo) * 0.65;
     subsurface = 0.6;
