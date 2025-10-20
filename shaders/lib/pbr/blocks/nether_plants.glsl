7a848c7197eeb5d10874f9cc228ed031e9226fdc
Septonious <septonious@proton.me>
2025-10-20 12:02:21 +0300

    Update nether_plants.glsl

--- shaders/lib/pbr/blocks/nether_plants.glsl
+++ shaders/lib/pbr/blocks/nether_plants.glsl
@@ -1,7 +1,7 @@
 else if (material == 64 || material == 81 || material == 82) {
     emission = int(lAlbedo > 0.9) * 0.25;
 } else if (material == 67) {
-    emission = pow16(lAlbedo) * 0.25;
+    emission = int(lAlbedo > 0.8) * lAlbedo * 0.01;
 } else if (material == 68) {
-    emission = pow7(lAlbedo) * 0.35;
+    emission = int(lAlbedo > 0.6) * lAlbedo * 0.01;
 }
