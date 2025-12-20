f7f6761491f30a773ca230a261918ca825d5282a
Septonious <septonious@proton.me>
2025-12-20 14:08:11 +0300

    Update aurora.glsl

--- shaders/lib/atmosphere/aurora.glsl
+++ shaders/lib/atmosphere/aurora.glsl
@@ -13,6 +13,7 @@ void drawAurora(inout vec3 color, in vec3 worldPos, in float VoU, in float caveF
 
 	kpIndex *= 1.0 + longPulse * 0.25;
 	kpIndex /= 9.0;
+	kpIndex =1;
 	visibility *= kpIndex * (1.0 + max(longPulse * 0.5, 0.0) + kpIndex * kpIndex * 0.5);
     visibility = min(visibility, 2.0) * AURORA_BRIGHTNESS;
 
@@ -30,19 +31,23 @@ void drawAurora(inout vec3 color, in vec3 worldPos, in float VoU, in float caveF
 		float currentStep = dither * sampleStep;
 
 		//Tilt factor. The stronger the geomagnetic storm, the less Aurora tilts towards the North
-		float tiltFactor = 0.05 + kpIndex * 0.15;
-		worldPos.xz += worldPos.y * tiltFactor;
+		float tiltFactor = 0.1 + kpIndex * 0.15;
+		worldPos.xz += worldPos.y * vec2(tiltFactor, tiltFactor * 2.);
 
 		//Altitude factor. Makes the aurora closer to you when you're ascending
 		float altitudeFactor = clamp(cameraPosition.y * 0.004, 0.0, 9.0);
 
         float accumulatedNoise = 0.0;
-        float northSouthStretching = 0.4 + pulse * 0.2;
-        float lineNoiseCoeff = 3.0 + pulse * 4.0; //[1.0 - 7.5]
-        float whirlNoiseCoeff = 25.0 + pulse * 25.0; //[20.0 - 100.0]
+        float northSouthStretching = 0.5;
+        float lineNoiseCoeff = 3.0; //[1.0 - 7.5]
+        float whirlNoiseCoeff = 2.0; //[20.0 - 100.0]
 
 		for (int i = 0; i < samples; i++) {
-			vec3 planeCoord = worldPos * ((20.0 + currentStep * (14.0 + accumulatedNoise * 9.0 + kpIndex * 5.0) - altitudeFactor) / worldPos.y) * 0.05;
+			vec3 planeCoord = worldPos * ((24.0 + currentStep * (14.0 + kpIndex * 5.0) - altitudeFactor) / worldPos.y) * 0.05;
+			vec2 offsetNoiseCoord = planeCoord.xz + cameraPosition.xz * 0.00005;
+			float offsetNoise = texture2D(noisetex, (offsetNoiseCoord + frameTimeCounter * 0.0001) * 0.025).r;
+				  offsetNoise = max(offsetNoise - 0.5, 0.0);
+				 planeCoord *= 0.5 + offsetNoise;
 			vec2 coord = planeCoord.xz + cameraPosition.xz * 0.0001;
 
 			//We don't want the aurora to render infintely, we also want it to be closer to the north when Kp is low
@@ -51,35 +56,35 @@ void drawAurora(inout vec3 color, in vec3 worldPos, in float VoU, in float caveF
 
 			if (auroraDistanceFactor > 0.0) {
                 coord.y *= northSouthStretching;
-                float baseOctaveA = texture2D(noisetex, coord * 0.002 + frameTimeCounter * 0.00006).b;
+                float stripesOctave = texture2D(noisetex, coord * 0.0025 + frameTimeCounter * 0.00003).b;
                 coord.y /= northSouthStretching;
-                float baseOctaveB = texture2D(noisetex, coord * 0.100 - frameTimeCounter * baseOctaveA * 0.00012).r;
-                float baseOctaveC_u = texture2D(noisetex, coord * 0.025 + frameTimeCounter * baseOctaveA * 0.00008 + 0.5).r;
-                float baseOctaveC = max(baseOctaveC_u - 0.5, 0.0);
-				float arcNoise = baseOctaveA * 6.0;
-					  arcNoise*= baseOctaveB * 4.0;
-					  arcNoise = max(1.0 - abs(arcNoise - 3.5 - baseOctaveB * baseOctaveB * lineNoiseCoeff - baseOctaveC * whirlNoiseCoeff), 0.0);
-					  arcNoise *= arcNoise * 0.5;
-                float blobNoise = max(0.0, baseOctaveB * baseOctaveB * (0.33 + baseOctaveA * 0.67) - 0.125);
-                float detailNoise = texture2D(noisetex, coord * 0.150 - frameTimeCounter * 0.0024).b;
-                float totalNoise = (arcNoise * (0.75 + detailNoise * 16.0 * baseOctaveC) + blobNoise * 0.75);
+                float detailOctave = texture2D(noisetex, coord * 0.250 - frameTimeCounter * 0.00009).r;
+				float midOctave = texture2D(noisetex, coord * 0.025 - frameTimeCounter * 0.00006).r;
+				float midOctaveM = max(midOctave - 0.45, 0.0);
+				float midOctaveM2 = max(midOctave - 0.55, 0.0);
+
+				float arcNoise = stripesOctave * 4.5 + detailOctave * 2.0;
+					  arcNoise = max(0.75 - 1.5 * abs(arcNoise - 3.0 - midOctaveM * 5.0), 0.0);
+					  arcNoise *= arcNoise * arcNoise * 0.75;
+
+				float blobNoise = midOctaveM * (0.25 + detailOctave * 2.75);
+					  blobNoise = max(blobNoise - 0.075 + stripesOctave * 0.125, 0.0) * detailOctave;
+
+                float totalNoise = arcNoise + blobNoise;
 
                 vec3 lowA = vec3(0.05, 1.55, 0.40);
-                vec3 upA = vec3(0.65 - baseOctaveC * baseOctaveC * 0.20, 0.30, 1.05 + baseOctaveC * baseOctaveC * 0.20);
+                vec3 upA = vec3(0.65, 0.30, 1.05);
                 vec3 auroraA = fmix(lowA, upA, pow(currentStep, 0.66));
-                     auroraA *= exp2(-(3.0 + baseOctaveC * 25.0) * i * sampleStep);
 
                 vec3 lowB = vec3(0.40, 1.55, 0.05);
-                vec3 upB = vec3(1.25 + baseOctaveC * pulse * 0.5, 0.05, 0.70);
+                vec3 upB = vec3(1.25, 0.05, 0.70);
                 vec3 auroraB = fmix(lowB, upB, pow(currentStep, 0.45));
-                     auroraB *= exp2(-(3.0 + baseOctaveC * 25.0) * i * sampleStep);
 
-                float colorMixer = min(max(baseOctaveC_u - 0.45 + kpIndex * 0.1, 0.0) * 16.0 * kpIndex * kpIndex, 1.0) * (0.25 + pulse * 0.75);
+                float colorMixer = 0.0;
 
-                vec3 totalAurora = mix(auroraA, auroraB, colorMixer);
+                vec3 totalAurora = mix(auroraA, auroraB, colorMixer) * exp2(-4.0 * i * sampleStep);
 
 				aurora += totalAurora * totalNoise * sqrt(auroraDistanceFactor);
-                accumulatedNoise += max(baseOctaveC * baseOctaveC * 25.0 - 0.075, 0.0);
 			}
 			currentStep += sampleStep;
 		}
