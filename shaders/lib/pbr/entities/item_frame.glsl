9314054ffcd42ce999b30c92d1b7ceef765aac0f
Septonious <septonious@proton.me>
2026-03-20 22:12:15 +0300

    glow item frame

--- shaders/lib/pbr/entities/item_frame.glsl
+++ shaders/lib/pbr/entities/item_frame.glsl
@@ -1,3 +1,6 @@
 else if (mat == 109) {
     lightmap.x *= 1.0 - lightmap.x * 0.5;
+} else if (mat == 110) {
+    albedo.a *= albedo.a;
+    albedo.rgb *= 2.0 + 2.0 * pow4(min(length(albedo.rgb), 1.0));
 }
